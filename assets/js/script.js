9b960ba88d91e045c9aebc0ad0fb8b5be1795ea8
ccorreas <ccorreas@MacBook-Air-de-Cristian.local>
2024-05-05 14:14:24 +0200

    CSS redesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,42 @@ const imagesLayout = 7;
 let imagesNumber;
 let forCounter;
 const layout = document.querySelector("#layout");
+const fileInput = document.getElementById('fileInput');
+const uploadButton = document.getElementById('uploadButton');
+const fileDetails = document.getElementById('fileDetails');
+const imagePreview = document.getElementById('imagePreview');
 let photosData = [];
+// Manejar el evento de cambio en el input de tipo file
+fileInput.addEventListener('change', handleFileSelect);
+
+// Función para manejar el archivo seleccionado
+function handleFileSelect(event) {
+    const file = event.target.files[0]; // Obtener el archivo seleccionado
+
+    // Verificar si el archivo seleccionado es una imagen
+    if (file && file.type.startsWith('image/')) {
+        // Crear un objeto FileReader
+        const reader = new FileReader();
+
+        // Definir la función de retorno de llamada cuando la lectura del archivo esté completa
+        reader.onload = function (e) {
+            // Crear una etiqueta de imagen y establecer su atributo src con los datos del archivo leído
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.classList.add("gallery__photo");
+
+            // Limpiar el contenedor de vista previa de imagen y agregar la imagen cargada
+            imagePreview.innerHTML = '';
+            imagePreview.appendChild(img);
+        };
+
+        // Leer el archivo como una URL de datos (data URL)
+        reader.readAsDataURL(file);
+    } else {
+        // Si el archivo seleccionado no es una imagen, mostrar un mensaje de error
+        imagePreview.innerHTML = 'Por favor, selecciona un archivo de imagen válido.';
+    }
+}
 
 
 const loadJSON = async () => {
@@ -71,38 +106,38 @@ const showImages = async () => {
 
 
         template += `
-    <section class="gallery__layout">
-        <div class="gallery1">
+    <section id="gallery-layout" class="gallery__layout">
+        <div class="gallery1 gallery">
             <div class="photo__title1">${photosData[grid1].ciudad}</div>
             <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid1].id}.png" alt="photo">
         </div>
 
-        <div class="gallery2">
+        <div class="gallery2 gallery">
         <div class="photo__title2">${photosData[grid2].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid2].id}.png" alt="photo">
         </div>
 
-        <div class="gallery3">
+        <div class="gallery3 gallery">
         <div class="photo__title3">${photosData[grid3].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid3].id}.png" alt="photo">
         </div>
 
-        <div class="gallery4">
+        <div class="gallery4 gallery">
         <div class="photo__title4">${photosData[grid4].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid4].id}.png" alt="photo">
         </div>
 
-        <div class="gallery5">
+        <div class="gallery5 gallery">
         <div class="photo__title5">${photosData[grid5].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid5].id}.png" alt="photo">
         </div>
 
-        <div class="gallery6">
+        <div class="gallery6 gallery">
         <div class="photo__title6">${photosData[grid6].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid6].id}.png" alt="photo">
         </div>
 
-        <div class="gallery7">
+        <div class="gallery7 gallery">
         <div class="photo__title7">${photosData[grid7].ciudad}</div>
         <img class="gallery__photo" src="./assets/img/gallery/${photosData[grid7].id}.png" alt="photo">
         </div>
@@ -112,8 +147,28 @@ const showImages = async () => {
 
 
 
+    const galleryLayout = document.querySelector("#gallery-items");
+    galleryLayout.innerHTML += template;
+    const viewClose = document.querySelector("#view-close");
 
-    layout.innerHTML += template;
+    const viewPhoto = document.querySelector("#view-photo");
+    const screenView = document.querySelector("#screen-view");
+
+    galleryLayout.addEventListener("click", (e) => {
+        const galleryPhoto = e.target.closest(".gallery__photo");
+        if (galleryPhoto) {
+            screenView.classList.add("screen__view--active");
+            viewPhoto.src = galleryPhoto.src;
+        }
+    })
+
+    viewClose.addEventListener("click", (e) => {
+        console.log("aa")
+        screenView.classList.remove("screen__view--active");
+    });
 };
 
 loadJSON();
+
+
+
